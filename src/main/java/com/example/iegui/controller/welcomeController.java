e9b7318a78e316bba35ee3ca64aaac2444d10cf6
michaelV04 <stvolmic@bx.fallmerayer.it>
2022-05-17 10:33:17 +0200

    welcome view pagination image update

--- src/main/java/com/example/iegui/controller/welcomeController.java
+++ src/main/java/com/example/iegui/controller/welcomeController.java
@@ -47,41 +47,23 @@ public class welcomeController extends Controller implements Initializable  {
         Text textFieldPage5 = new Text();
 
 
-        ImageView imageView = new ImageView();
-        ImageView imageViewcooleTypn = new ImageView();
+        ImageView imageViewcooleTypn = addimage("Planning/Images/CooleTypen.jpg");
+        ImageView imageView = addimage("Planning/Images/MainView.png");
+        ImageView imageViewsettings = addimage("Planning/Images/SettingView.png");
 
-        try {
-            FileInputStream inputstream = new FileInputStream("Planning/Images/MainView.png");
-            Image image = new Image(inputstream);
-            imageView.setImage(image);
-            imageView.setFitWidth(800);
-            imageView.setFitHeight(800);
-            imageView.setPreserveRatio(true);
-            imageView.setSmooth(true);
-            imageView.setCache(true);
-        } catch (Exception ex) {
-            ex.getMessage();
-        }
 
-        try{
-            FileInputStream inputstreamcooleTypn = new FileInputStream("Planning/Images/CooleTypen.jpg");
-            Image imagecooleTypn = new Image(inputstreamcooleTypn);
-            imageViewcooleTypn.setImage(imagecooleTypn);
-            imageViewcooleTypn.setFitWidth(800);
-            imageViewcooleTypn.setFitHeight(800);
-            imageViewcooleTypn.setPreserveRatio(true);
-            imageViewcooleTypn.setSmooth(true);
-            imageViewcooleTypn.setCache(true);
-        }catch(Exception e){
-            e.getMessage();
-        }
         VBox pageBoxMain = new VBox();
         VBox pagBoxWelcome = new VBox();
+        VBox pageBoxSettings = new VBox();
+
         pageBoxMain.getChildren().add(textFieldPage2);
         pageBoxMain.getChildren().add(imageView);
 
         pagBoxWelcome.getChildren().add(textFieldPage1);
         pagBoxWelcome.getChildren().add(imageViewcooleTypn);
+
+        pageBoxSettings.getChildren().add(textFieldPage3);
+        pageBoxSettings.getChildren().add(imageViewsettings);
 
         try {
             textFieldPage1.textProperty().bind(Language.get("welcome"));
@@ -123,12 +105,29 @@ public class welcomeController extends Controller implements Initializable  {
                 switch (integer){
                     case 0: return pagBoxWelcome;
                     case 1: return pageBoxMain;
-                    case 2: return textFieldPage3;
+                    case 2: return pageBoxSettings;
                     case 3: return textFieldPage4;
                     case 4: return textFieldPage5;
                     default: return null;
                 }
             }
         });
+    }
+
+    public ImageView addimage(String url){
+        ImageView imageView = new ImageView();
+        try{
+            FileInputStream inputstream = new FileInputStream(url);
+            Image image = new Image(inputstream);
+            imageView.setImage(image);
+            imageView.setFitWidth(600);
+            imageView.setFitHeight(600);
+            imageView.setPreserveRatio(true);
+            imageView.setSmooth(true);
+            imageView.setCache(true);
+        }catch(Exception e){
+            e.getMessage();
+        }
+        return imageView;
     }
 }
