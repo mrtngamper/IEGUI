0c064c291f1a47e977f5fd0ead6c9b326a6af772
Maxnboy <maximilianp0303@gmail.com>
2022-05-25 15:14:56 +0200

    method display 1

--- src/main/java/com/example/iegui/MainApplication.java
+++ src/main/java/com/example/iegui/MainApplication.java
@@ -1,36 +1,28 @@
 package com.example.iegui;
 
 
-import com.example.iegui.AI.ImageEnhanceMethod;
-import com.example.iegui.AI.SwinIR;
 import com.example.iegui.util.Context;
 import com.example.iegui.util.Controller;
-import com.example.iegui.util.Language;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 import java.io.IOException;
 import java.util.Locale;
 
 public class MainApplication extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        Context context = new Context(stage,"Settings/settings.yml");
+        Context context = new Context(stage, "Settings/settings.yml");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 500, 350);
+        Scene scene = new Scene(root, 768, 500);
 
         String os = System.getProperty("os.name", "generic").toLowerCase(Locale.US);
         if (os.equals("mac os x")) {
@@ -41,18 +33,14 @@ public class MainApplication extends Application {
         controller.setContext(context);
 
 
-       // context.getMethods().get(0).start("/home/martin/IdeaProjects/benutzerveerwaltung/Junit/IEGUI/EnhanceMethod/SwinIR/katze.jpg","/home/martin/IdeaProjects/benutzerveerwaltung/Junit/IEGUI/EnhanceMethod/SwinIR/katzeout.jpg");
+        // context.getMethods().get(0).start("/home/martin/IdeaProjects/benutzerveerwaltung/Junit/IEGUI/EnhanceMethod/SwinIR/katze.jpg","/home/martin/IdeaProjects/benutzerveerwaltung/Junit/IEGUI/EnhanceMethod/SwinIR/katzeout.jpg");
 
 
-        stage.setFullScreen(true);
-        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        //stage.setFullScreen(true);
+        //stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.setResizable(false);
         stage.setTitle("IEGUI");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,5 @@ module com.example.iegui {
     opens com.example.iegui.controller to javafx.fxml;
     opens com.example.iegui to javafx.fxml;
     exports com.example.iegui;
+    opens com.example.iegui.CustomNodes to javafx.fxml;
 }
