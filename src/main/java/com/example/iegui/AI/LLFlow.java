4df75f912545a8bfe99b03a12f6326a416219ea1
kilian <kierkilian@gmail.com>
2022-06-02 14:40:35 +0200

    added missing files

---
+++ src/main/java/com/example/iegui/AI/LLFlow.java
@@ -0,0 +1,34 @@
+package com.example.iegui.AI;
+
+import com.example.iegui.util.Context;
+
+import java.io.File;
+
+public class LLFlow extends ImageEnhanceMethod{
+    /**
+     * Upon object creation the method directory is being stored and method settings are loaded from the Config folder.
+     *
+     * @param location The method location
+     * @param lang     The language which should be loaded
+     * @param context
+     */
+    public LLFlow(String location, String lang, Context context) {
+        super(location, lang, context);
+    }
+
+    @Override
+    public String[] getCMD() {
+        String environment =  new File("Environments"+"/"+getEnvironment()).getAbsolutePath();
+        return new String[]{
+                environment + "/bin/python3",
+                "test_unpaired.py",
+                "--opt",
+                "confs/LOLv2-pc.yml",
+                "--input",
+                context.getTempdir()+"/input",
+                "--output",
+                context.getTempdir()+"/output"
+                // "python test_unpaired.py --opt confs/LOLv2-pc.yml -n result"
+        };
+    }
+}
