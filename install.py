e59e3ed5b4b96a7681d2500153dc25dd865bf673
mrtn <mrtngamper@gmail.com>
2022-06-03 14:19:11 +0200

    Installation script

---
+++ install.py
@@ -0,0 +1,99 @@
+import yaml
+import shutil
+import os
+import argparse
+import pathlib
+import os
+import zipfile
+
+def normalize(raw_path):
+    return pathlib.Path(raw_path).expanduser().resolve().__str__()
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--zip", type=str, default=None)
+parser.add_argument("--installation", type=str, default=None)
+parser.add_argument("--model", type=str, default="model")
+parser.add_argument("--source",type=str, default ="./")
+
+args= parser.parse_args()
+
+directory = normalize(args.model)
+source = normalize(args.source)
+
+
+
+tempdir = source+"/tempInstallationDir"
+def main():
+    global tempdir
+    if(args.installation!=None):
+        tempdir=args.installation
+
+    if(args.zip != None or args.installation !=None ):
+
+        os.system("mvn install")
+        try:
+            os.makedirs(tempdir)
+        except:
+            print("Temp dir exists")
+        print("copying jar")
+        shutil.copyfile(source+"/target/IEGUI-1.0-SNAPSHOT-shaded.jar",tempdir+"/iegui.jar")
+        print("copying EnhanceMethod")
+        shutil.copytree(source+"/EnhanceMethod/", tempdir+"/EnhanceMethod/", dirs_exist_ok=True, ignore=shutil.ignore_patterns('*.pth'))
+        print("copying Images")
+        shutil.copytree(source+"/Images/", tempdir+"/Images/", dirs_exist_ok=True,ignore=shutil.ignore_patterns('*.pth'))
+        print("copying Language")
+        shutil.copytree(source+"/Language/", tempdir+"/Language/", dirs_exist_ok=True,ignore=shutil.ignore_patterns('*.pth'))
+        #TODO Remove Planning when it is noot needed for images anymore
+        print("copying Planning")
+        shutil.copytree(source+"/Planning/", tempdir+"/Planning/", dirs_exist_ok=True,ignore=shutil.ignore_patterns('*.pth'))
+        print("copying Environments")
+        files=os.listdir(source+"/Environments/")
+        try:
+            os.makedirs(tempdir+"/Environments")
+        except:
+            print("Environments dir exists")
+        for fname in files:
+            if(not os.path.isdir(source+"/Environments/"+fname)):
+                shutil.copy2(os.path.join(source+"/Environments/",fname), tempdir+"/Environments/"+fname)
+        print("copying models")
+        copyModels(tempdir)
+
+        if(args.zip!=None):
+            print("creating zip archive")
+            shutil.make_archive(os.path.splitext(args.zip)[0], 'zip', tempdir)
+            if(args.installation!=None):
+                shutil.rmtree(tempdir)
+
+    else:
+        copyModels(source)
+
+
+
+def copyModels(destination):
+    if (not os.path.isdir(directory)):
+        print("Model directory not found: " + directory)
+        exit(-1)
+
+    try:
+        with open(directory+'/location.yml', 'r') as file:
+            yml = yaml.safe_load(file)
+            for i in yml:
+                try:
+                    shutil.copy(directory+"/"+i,destination+"/"+yml[i]+"/")
+                except Exception as f:
+                    print("copying: "+i)
+                    print(i + ", " + yml[i]+": Could not be copied")
+                    print(f)
+    except Exception as e:
+        print(e)
+
+
+
+
+
+
+main()
+
+
--- src/main/java/com/example/iegui/MainApplication.java
+++ src/main/java/com/example/iegui/MainApplication.java
@@ -1,6 +1,7 @@
 package com.example.iegui;
 
 
+import com.example.iegui.util.Alerts;
 import com.example.iegui.util.Context;
 import com.example.iegui.util.Controller;
 import javafx.application.Application;
@@ -21,18 +22,69 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Locale;
 
+import com.sun.jna.Native;
+
+import com.sun.jna.Library;
+
 public class MainApplication extends Application {
+
+
     public static void main(String[] args) {
+
         launch();
     }
 
+    /**
+     * Sets working directory under windows
+     */
+    private static interface MyKernel32 extends Library {
+        public MyKernel32 INSTANCE = (MyKernel32) Native.loadLibrary("Kernel32", MyKernel32.class);
+
+        /** BOOL SetCurrentDirectory( LPCTSTR lpPathName ); */
+        int SetCurrentDirectoryW(String pathName);
+    }
+
+    /**
+     * Sets workingrectory under linux
+     */
+    private interface MyCLibrary extends Library {
+        MyCLibrary INSTANCE = (MyCLibrary) Native.loadLibrary("c", MyCLibrary.class);
+
+        /** int chdir(const char *path); */
+        int chdir( String path );
+    }
+
+
     @Override
     public void start(Stage stage) throws IOException {
+        if(!new File("EnhanceMethod").exists()) {
+            String workingDir = "";
+            try {
+
+                workingDir = new File(MainApplication.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+            } catch (URISyntaxException e) {
+                System.out.println(e.getMessage());
+            }
+
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                MyKernel32 library = MyKernel32.INSTANCE;
+                library.SetCurrentDirectoryW(workingDir);
+            } else {
+                MyCLibrary library = MyCLibrary.INSTANCE;
+                System.out.println(library.chdir(workingDir));
+            }
+            System.setProperty("user.dir", workingDir);
+            System.setProperty("user.home", workingDir);
+        }
+
         Context context = new Context(stage, "Settings/settings.yml");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
         Parent root = fxmlLoader.load();
@@ -48,7 +100,7 @@ public class MainApplication extends Application {
         //context.getMethods().get(2).start("/home/martin/Downloads/output14.png","/home/martin/Downloads/output15.png"); // For Testing purposes white balance
       //  context.getMethods().get(3).start("/home/martin/Downloads/IMG-20220601-WA0000.jpg","/home/martin/Downloads/output13.png"); // For Testing purposes white balance
        // context.getMethods().get(5).start("/home/martin/Downloads/bild6.jpg","/home/martin/Downloads/output14.png"); // For Testing purposes white balance
-         context.getMethods().get(4).start("/home/martin/Downloads/lowresselfie.jpg","/home/martin/Downloads/output15.png"); // For Testing purposes white balance
+        // context.getMethods().get(4).start("/home/martin/Downloads/lowresselfie.jpg","/home/martin/Downloads/output15.png"); // For Testing purposes white balance
 
         stage.setTitle("IEGUI");
         stage.setScene(scene);
